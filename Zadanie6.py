d13d78157caad87910203253ba9874b4560a5476
FilipoPoland <filipopoland@wp.pl>
2023-04-18 15:59:33 +0200

    Commit

--- Zadanie6.py
+++ Zadanie6.py
@@ -1,13 +1,33 @@
- # napisz program tworzacy liste z elemtow podanych przez uzytkownika
- # lista ma posiadac 5 elemtow
+# napisz program tworzacy liste z elemtow podanych przez uzytkownika
+# lista ma posiadac 5 elemtow
 
- list1 = []
- for i in range(5):
-     u_input = input(f'Podaj wartość do {i} pozycji listy: ')
-try:
-int
-float
-Bool
-list
-str
-     list1.append()
+list1 = []
+print('Program prosi o 5 wartości które będzie próbował zrozumieć:\n'
+      '- Dla tekstu, wartości logicznych poprostu go/je wprowadz,\n'
+      '- Liczby należy wprowadzić używając kropki jako separatora dziesiętnego\n'
+      '  w innym razie będzie zaklasyfikowana jako tekst,\n')
+
+for i in range(5):
+    u_input = input(f'Podaj {i + 1} wyrażenie w liście: ')
+    try:
+        interpretation = int(u_input)
+        list1.append(interpretation)
+    except:
+        try:
+            interpretation = float(u_input)
+            list1.append(interpretation)
+        except:
+            if u_input == 'True':
+                interpretation = True
+                list1.append(interpretation)
+            elif u_input == 'False':
+                interpretation = False
+                list1.append(interpretation)
+            else:
+                interpretation = str(u_input)
+                list1.append(interpretation)
+
+for j in range(5):
+    print(type(list1[j]))
+
+print(list1)
